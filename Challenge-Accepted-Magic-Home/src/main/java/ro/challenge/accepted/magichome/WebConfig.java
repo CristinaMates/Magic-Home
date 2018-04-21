0ba18338dc32baf24d7dffbd895962ef7af686af
prese <sebastian@devm8.com>
2018-04-21 12:25:00 +0300

    structure

--- Challenge-Accepted-Magic-Home/src/main/java/ro/challenge/accepted/magichome/WebConfig.java
+++ Challenge-Accepted-Magic-Home/src/main/java/ro/challenge/accepted/magichome/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @Configuration
 @EnableWebMvc
 public class WebConfig {
-    @Override
+
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(
                 "/webjars/**", "/resources/**",
