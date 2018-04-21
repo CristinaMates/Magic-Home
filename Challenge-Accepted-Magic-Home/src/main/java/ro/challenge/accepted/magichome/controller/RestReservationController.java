bb4ae3dd891b4804d9b6aebc2707928c4f59b050
tudor.patrutiu <patrutiu.tudor@gmail.com>
2018-04-21 12:56:25 +0300

    implemented a create method in rest controller

--- Challenge-Accepted-Magic-Home/src/main/java/ro/challenge/accepted/magichome/controller/RestReservationController.java
+++ Challenge-Accepted-Magic-Home/src/main/java/ro/challenge/accepted/magichome/controller/RestReservationController.java
@@ -1,10 +1,13 @@
 package ro.challenge.accepted.magichome.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ro.challenge.accepted.magichome.domain.Doctor;
+import ro.challenge.accepted.magichome.domain.Patient;
 import ro.challenge.accepted.magichome.domain.Reservation;
 import ro.challenge.accepted.magichome.dto.CreatePacientReservation;
 import ro.challenge.accepted.magichome.service.ReservationService;
@@ -14,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservation")
-public class RestPatientController {
+public class RestReservationController {
     @Autowired
     private ReservationService reservationService;
 
@@ -28,12 +31,11 @@ public class RestPatientController {
 
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String create(@PathVariable CreatePacientReservation dto) {
-        //from dto to reservation
+    public String create(@PathVariable CreatePacientReservation dto, Model model) {
+        Reservation reservation = getReservation(dto);
+        reservationService.createReservation(reservation);
         // /reservationService.createReservation(null);///new Reservation());
-
         return "OK";
-
     }
 
 
@@ -51,5 +53,23 @@ public class RestPatientController {
         // // smsService.sendSms();
 
         return "OK";
+    }
+    private Reservation getReservation (CreatePacientReservation dto){
+        Reservation reservation = new Reservation();
+        reservation.setDays(dto.getPeriod());
+
+        Patient patient = new Patient();
+        patient.setFirstName(dto.getPacient().getFirstName());
+        patient.setPatientLastName(dto.getPacient().getPatientLastName());
+        patient.setDiagnosis(dto.getDiagnosis());
+        patient.setAge(dto.getPacient().getAge());
+        patient.setDisability(dto.isDisability());
+        
+        Doctor doctor = new Doctor();
+
+        reservation.setPatient(patient);
+        reservation.setDoctor(doctor);
+        return reservation;
+
     }
 }
--- Challenge-Accepted-Magic-Home/src/main/java/ro/challenge/accepted/magichome/service/ReservationService.java
+++ Challenge-Accepted-Magic-Home/src/main/java/ro/challenge/accepted/magichome/service/ReservationService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ReservationService {
 
-    void createReservation(ReservationService reservationService);
+    void createReservation(Reservation reservation);
 
     List<Reservation> getAll();
 }
